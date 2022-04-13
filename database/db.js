d942deea63edc030bde3bdf13b0c52137bd79e6d
Tabatha Gamboa <tabathagamboa9@gmail.com>
2022-04-12 21:10:24 -0400

    migracion base de datos

---
+++ database/db.js
@@ -0,0 +1,20 @@
+const pool = require("./migrate")
+
+const geProductosDB = async () => {
+    const client = await pool.connect()
+    try{
+        const respuesta = await client.query("SELECT * FROM Productos;")
+        return {
+            ok:true,
+            msg: respuesta.rows,
+        }
+    }catch(e){
+        console.log(e)
+        return {
+            ok:false,
+            msg: error.message ,
+        }
+    }finally{
+        client.release()
+    }
+}
